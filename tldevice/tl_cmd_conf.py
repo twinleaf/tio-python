4ae82af4b2a43da963f38dc1f56e119b112452df
Tom Kornack <kornack@twinleaf.com>
2018-05-18 13:15:06 -0400

    Fix error on device mismatch.

--- tldevice/tl_cmd_conf.py
+++ tldevice/tl_cmd_conf.py
@@ -38,8 +38,8 @@ class TwinleafConfigController(object):
     configuration = {}
     functions = self._enum()
     for function, function_type in functions:
-        value = self._dev._tio.rpc_val(function, rpcType=function_type)
-        configuration[function] = value
+      value = self._dev._tio.rpc_val(function, rpcType=function_type)
+      configuration[function] = value
     document = {
       'Name': self._dev.dev.name(),
       'Revision': self._dev.dev.revision(),
@@ -49,9 +49,9 @@ class TwinleafConfigController(object):
     }
     document['Configuration'] = configuration
     if filename:
-        stream = open(filename, 'w')
-        yaml.dump(document, stream, default_flow_style=False)
-        print('Wrote configuration to file %s' % filename)
+      stream = open(filename, 'w')
+      yaml.dump(document, stream, default_flow_style=False)
+      print('Wrote configuration to file %s' % filename)
     return configuration
 
   def upload(self, filename="config.yaml"):
@@ -73,37 +73,37 @@ class TwinleafConfigController(object):
     mcu_fw_rev = self._dev.dev.firmware.rev()
 
     if document['Name'] != dev_name:
-        print(dev_desc, document['Name'])
-        raise Exception(f"Device mismatch: device is '{dev_name}'; file is '{document['Name']}'.")
+      #print(dev_name, document['Name'])
+      raise Exception(f"Device mismatch: device is '{dev_name}'; file is '{document['Name']}'.")
 
     if document['Revision'] != revision:
-        print(f"ID mismatch: device is {revision}; file is {document['Revision']}")
+      print(f"ID mismatch: device is {revision}; file is {document['Revision']}")
 
     if document['Serial'] != serial:
-        print(f"ID mismatch: device is {serial}; file is {document['Serial']}")
+      print(f"ID mismatch: device is {serial}; file is {document['Serial']}")
 
     if document['ID'] != mcu_id:
-        print(f"ID mismatch: device is {mcu_id}; file is {document['ID']}")
+      print(f"ID mismatch: device is {mcu_id}; file is {document['ID']}")
 
     if document['Firmware'] != mcu_fw_rev:
-        print(f"Firmware mismatch: device is {mcu_fw_rev}; file is {document['Firmware']}")
+      print(f"Firmware mismatch: device is {mcu_fw_rev}; file is {document['Firmware']}")
 
     for function in document['Configuration'].keys():
-        if function not in functionValues.keys():
-            print(f'Skipping configuration for {function}; variable not available on device.')
+      if function not in functionValues.keys():
+        print(f'Skipping configuration for {function}; variable not available on device.')
 
     for function, function_type in functions:
-        if function in list(document['Configuration'].keys()):
-            valueConfig = document['Configuration'][function]
-            valueDevice = functionValues[function]
-            if valueDevice == valueConfig:
-                print(f'Skipping configuration for {function}; value {valueConfig} not changed.')
-            else:
-                try: 
-                    self._dev._tio.rpc_val(function, function_type, valueConfig)
-                    print(f'Changing configuration for {function} from {valueDevice} to {valueConfig}.')
-                except:
-                    print(f'Error changing configuration for {function} from {valueDevice} to {valueConfig}.')
+      if function in list(document['Configuration'].keys()):
+        valueConfig = document['Configuration'][function]
+        valueDevice = functionValues[function]
+        if valueDevice == valueConfig:
+          print(f'Skipping configuration for {function}; value {valueConfig} not changed.')
         else:
-            print(f'Skipping configuration for {function}; value not provided (is it a new variable on device?).')
+          try: 
+            self._dev._tio.rpc_val(function, function_type, valueConfig)
+            print(f'Changing configuration for {function} from {valueDevice} to {valueConfig}.')
+          except:
+            print(f'Error changing configuration for {function} from {valueDevice} to {valueConfig}.')
+      else:
+        print(f'Skipping configuration for {function}; value not provided (is it a new variable on device?).')
 
