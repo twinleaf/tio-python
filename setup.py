757a93d20e4a46a2aba0cb112d71045617fca900
Tom Kornack <kornack@twinleaf.com>
2021-06-09 17:48:29 -0400

    Add back setup.py for older python systems

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# Until PEP 517 is more widespread, we use this stub to provide backward compatibility
+# install using, for example, `python3 setup.py install`
+import setuptools
+setuptools.setup()
